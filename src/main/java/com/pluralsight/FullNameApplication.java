d2f47f066998f4a754eb7ea39a058c823bf21f35
DomaniqueK <domaniqueking24@gmail.com>
2025-09-29 14:22:52 -0500

    Created name application

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Please enter your last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Please enter your first name:");
+        String middleName = scanner.nextLine();
+        String name = firstName.trim();
+        if (!middleName.isBlank());
+        name = name + " " + middleName.trim();
+
+
+        System.out.println("Please enter your suffix:");
+        String suffix = scanner.nextLine();
+        if (!suffix.isBlank())
+            name = name + ", " + suffix.trim();
+
+
+        name = name + " " + lastName.trim() + suffix;
+
+
+
+    }
+}
